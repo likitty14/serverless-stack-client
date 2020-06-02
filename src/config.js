de27cd290812f51901c35bd537d52baf129ba7d2
Kitty Li <kitty.li@ha.org.hk>
2020-06-02 11:22:01 +0800

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "notes-app-upload-kitty"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://vxnz2rjzrj.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_QK7x2RhwF",
+      APP_CLIENT_ID: "5d8ee1jfpv58pjlkatbl9opjag",
+      IDENTITY_POOL_ID: "us-east-2:9b99213a-e64e-4aaf-a48c-80ee51f4ca3d"
+    }
+  };
